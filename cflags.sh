b4b4f4ce45009e785a1732f7e1c56710c207408f
kirill <Mom's Engineer>
2020-05-31 02:24:13 +0300

    M-2 | Final version

--- cflags.sh
+++ cflags.sh
@@ -18,39 +18,45 @@
 		-f [file]          or --file   --> create a file
 --Comment--
 
-if (( 1 > $# )); then
-	echo "Too few arguments"
-	exit 0
-fi
-
 case $1 in
-	# Create a symbol link
 	-l|--link)
 		if (( 3 > $# )) || [[ ! -e $2 ]] || [[ -e $3 ]]; then
 			echo "Some problems with link creation"
-			exit 0
+			exit 23
 		fi
 		ln -s $2 $3
 	;;
 
-	# Remove a file
 	-r|--remove)
 		if (( 2 > $# )) || [[ ! -f $2 ]]; then
-			echo "Some problems with link creation"
-			exit 0
+			echo "Some problems with file removing"
+			exit 24
 		fi
 		rm $2
 	;;
 
-	# Create a file
 	-f|--file)
 		if (( 2 > $# )) || [[ -e $2 ]]; then
-			echo "Some problems with link creation"
-			exit 0
+			echo "Some problems with file creation"
+			exit 25
 		fi
 	;;
+
+	-h|--help)
+		echo "Help:"
+		echo "    -h                 or --help   --> invoke help list"
+		echo "    -l [target] [link] or --link   --> create a symbol link"
+		echo "    -r [file]          or --remove --> remove a file"
+		echo "    -f [file]          or --file   --> create a file"
+		echo -e "\nExit codes: "
+		echo "   23 --> Problem with link creation"
+		echo "   24 --> Problem with file removing"
+		echo "   25 --> Problem with file creation"
+		echo "   26 --> Invalid parameters"
+	;;
+
 	*)
-		echo "Wrong flag entered. There is no such flag ($1)"
-		exit 0
+		echo "Wrong flag entered. There is no such flag ($1). Use -h or --help."
+		exit 26
 	;;
 esac
